ad8c1f1462bd0a46bff1d77c040322034abb3016
abdrahamanecisse <cisseabdouibtis@yahoo.fr>
2025-02-11 14:32:56 +0100

    first commit

---
+++ src/check.Tools/ReactDevTools.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReactDevTools = () => {
+  return (
+    <div>
+    <p> c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-01-09 143214.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 121555.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 121904.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 122334.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 123424.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 123824.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 124050.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 124125.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 124452.png c:\Users\dali_\OneDrive\Bureau\React-Dev-Tools\Capture d'écran 2025-02-11 124635.png</p>
+    </div>
+  )
+}
+
+export default ReactDevTools
